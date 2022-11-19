3fa8db8b8eae5b8efcd3f0abba97968bb02cb5fa
Sharafudheenkamar <sharafudheenkamar@gmail.com>
2022-11-19 10:19:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title="dom";
+// console.log(document.title);
+// //document.body.h1="Welcome all";
+// console.log(document.all);
+// console.log(document.getElementById("heading"));
+// console.log(document.getElementById("heading").textContent);
+
+// console.log(document.getElementById("heading").innerText);
+
+// console.log(document.getElementById("heading").innerHTML);
+// console.log(document.getElementById("dom").textContent);
+
+// console.log(document.getElementById("dom").innerText);
+
+// console.log(document.getElementById("dom").innerHTML);
+// console.log(document.getElementsByClassName("heading"));
+// let hold=document.getElementsByClassName("heading");
+// console.log(hold[0].textContent);
+// hold[0].style.color="red";
+// hold[1].style.fontFamily="Calibri";
+for(i=0;i<=HTMLBodyElement.length;i++){
+HTMLBodyElement[i].style.color="red";
+};
+let hold=document.getElementsByTagName("h1");
+console.log(hold[0]);
